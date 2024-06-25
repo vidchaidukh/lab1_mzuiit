9290632d225c18af02fdc50984a5410361c2a534
root <root@eugenelaptop>
2024-06-26 00:59:04 +0300

    add security group

--- main.tf
+++ main.tf
@@ -17,11 +17,37 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH traffic"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-
+  key_name = "key_mzuiit"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
   tags = {
     Name = "HelloWorld"
   }
