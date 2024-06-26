cd750d8cddea9382083a01fa49a80e269448f209
root <root@eugenelaptop>
2024-06-26 12:30:16 +0300

    pipeline is working, docker not

--- main.tf
+++ main.tf
@@ -36,25 +36,29 @@ resource "aws_instance" "web" {
     Name = "lab2_mzuiit"
   }
     user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
-sudo service docker start
-sudo usermod -a -G docker ubuntu
+	#!/bin/bash
+	# install and configure docker on the ec2 instance
+	# Add Docker's official GPG key:
+sudo echo 'APT::Get::Assume-Yes;' | sudo tee -a /etc/apt/apt.conf.d/00Do-not-ask
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https
+sudo install -y -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-              sudo docker run -d --name my-web-app -p 80:80 collider41/my-web-app:latest
-              sudo docker run -d \
+# Add the repository to Apt sources:
+sudo echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras libltdl7 libslirp0 pigz
+  slirp4netns        
+sudo docker run -d --name my-web-app -p 80:80 collider41/my-web-app:latest
+        sudo docker run -d \
                 --name watchtower \
                 -v /var/run/docker.sock:/var/run/docker.sock \
                 containrrr/watchtower \
                 --interval 300
-              EOF
+        EOF
 }
