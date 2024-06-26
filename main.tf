ad8d10291553f3a726a7eeed4780e1a2beed5e6d
root <root@eugenelaptop>
2024-06-26 03:16:21 +0300

    another method

--- main.tf
+++ main.tf
@@ -37,13 +37,19 @@ resource "aws_instance" "web" {
   }
     user_data = <<-EOF
               #!/bin/bash
-              sudo mkdir lab2
-              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-              sudo touch test.txt
-              sudo snap install docker
-              sudo systemctl enable docker
-              sudo systemctl start docker
-              sudo chown $USER /var/run/docker.sock
+              sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+sudo service docker start
+sudo usermod -a -G docker ubuntu
+
               sudo docker run -d --name my-web-app -p 80:80 collider41/my-web-app:latest
               sudo docker run -d \
                 --name watchtower \
